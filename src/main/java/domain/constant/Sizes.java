16a50159e6680d246ebdf14acdff45db376a0b03
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-10-26 16:48:41 +0900

    잘못된 사이즈 수정

--- src/main/java/domain/constant/Sizes.java
+++ src/main/java/domain/constant/Sizes.java
@@ -3,7 +3,7 @@ package domain.constant;
 public class Sizes {
 
 	public static final int SCREEN_WIDTH = 1040;
-	public static final int SCREEN_HEIGHT = 780;
+	public static final int SCREEN_HEIGHT = 808;
 	
 	public static final int CELL_WIDTH_COUNT = 15;
 	public static final int CELL_HEIGHT_COUNT = 13;
